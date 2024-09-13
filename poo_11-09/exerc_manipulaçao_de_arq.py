415a0e6219ee8a0fa79038c9430577269ff0b526
Ckenz0 <146033211+Ckenz0@users.noreply.github.com>
2024-09-13 18:48:50 -0300

    hmmmmmmmmmmmm

--- poo_11-09/exerc_manipulaçao_de_arq.py
+++ poo_11-09/exerc_manipulaçao_de_arq.py
@@ -10,12 +10,21 @@ class GerenciarArquivos:
             self.funcionarios = {}
     
     def inserir_func(self, nome, cargo):
-        super().__init__(nome, cargo)
         with open("poo_11-09/funcionarios.txt", "w", encoding="utf-8") as f:
             f.write(f"Nome: {nome}, Cargo: {cargo}")
 
     def listar_func(self):
-        pass
-   
+        with open("poo_11-09/funcionarios.txt", "r", encoding="utf-8") as f:
+            conteudo = f.read()
+            print(conteudo)
 
+    def imprimir_info(self):
+        print(f"Nome: {self.nome}, Cargo: {self.cargo}")
+    
+
+gerenciador = GerenciarArquivos("Caue", "Developer", "poo_11-09/funcionarios.txt")
+gerenciador.inserir_func("João", "Analista")
+gerenciador.inserir_func("Maria", "Gerente")
+gerenciador.listar_func()
+gerenciador.imprimir_info()
     
