7aea3f7734f5bfe32c1e8440d8e62590bee075ee
Ckenz0 <146033211+Ckenz0@users.noreply.github.com>
2024-09-27 18:07:13 -0300

    Arrumando metodos

--- poo_25-09/sistema_gerenciamento_veiculos.py
+++ poo_25-09/sistema_gerenciamento_veiculos.py
@@ -45,19 +45,21 @@ def main():
     caminhao1 = Caminhao("Scania Super", 2021, 250000)
     caminhao2 = Caminhao("Sprinter 417", 2020, 200000)
 
-    carro1_seguro = carro1.calcular_seguro
-    carro2_seguro = carro2.calcular_seguro
-    moto1_seguro = moto1.calcular_seguro
-    moto2_seguro = moto2.calcular_seguro
-    caminhao1_seguro = caminhao1.calcular_seguro
-    caminhao2_seguro = caminhao2.calcular_seguro
+    carro1_seguro = carro1.calcular_seguro()
+    carro2_seguro = carro2.calcular_seguro()
+    moto1_seguro = moto1.calcular_seguro()
+    moto2_seguro = moto2.calcular_seguro()
+    caminhao1_seguro = caminhao1.calcular_seguro()
+    caminhao2_seguro = caminhao2.calcular_seguro()
 
-    print(f"{carro1} possui valor de seguro de {carro1_seguro}")
-    print(f"{carro2} possui valor de seguro de {carro2_seguro}")
-    print(f"{moto1} possui valor de seguro de {moto1_seguro}")
-    print(f"{moto2} possui valor de seguro de {moto2_seguro}")
-    print(f"{caminhao1} possui valor de seguro de {caminhao1_seguro}")
-    print(f"{caminhao2} possui valor de seguro de {caminhao2_seguro}")
+    print(f"O valor de seguro para o carro {carro1.modelo} de {carro1.ano} avaliado em {carro1.valor} reais é de {carro1_seguro}")
+    print(f"O valor de seguro para o carro {carro2.modelo} de {carro2.ano} avaliado em {carro2.valor} reais é de {carro2_seguro}")
+    print(f"O valor de seguro para o carro {moto1.modelo} de {moto1.ano} avaliado em {moto1.valor} reais é de {moto1_seguro}")
+    print(f"O valor de seguro para o carro {moto2.modelo} de {moto2.ano} avaliado em {moto2.valor} reais é de {moto2_seguro}")
+    print(f"O valor de seguro para o carro {caminhao1.modelo} de {caminhao1.ano} avaliado em {caminhao1.valor} reais é de {caminhao1_seguro}")
+    print(f"O valor de seguro para o carro {caminhao2.modelo} de {caminhao2.ano} avaliado em {caminhao2.valor} reais é de {caminhao2_seguro}")
+
+    Veiculo.total_veiculos()
 
 if __name__ == '__main__':
     main()
